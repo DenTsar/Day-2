7ada32244da5741fd0b61d5bd863de329687ccb7
DenTsar <45399002+DenTsar@users.noreply.github.com>
2018-11-29 12:19:40 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 public class Day2{
 	public static void main(String[]args){
 		System.out.println(convertToArrayList(8950));
-		//System.out.println(reconstruct(convertToArrayList(8950)));
+		System.out.println(reconstruct(convertToArrayList(8950)));
 	}
 	public static ArrayList<Integer> convertToArrayList(int a){
 		ArrayList<Integer> b = new ArrayList<>();
@@ -13,15 +13,12 @@ public class Day2{
 	}
 	public static int reconstruct(ArrayList<Integer> a){
 		int original = 0;
-
 		for (int i = 0; i<a.size(); i++){
 			if (i==0)
 				original+=a.get(i);
 			else
 				original+=(a.get(i)*10*i);
 		}
-
 		return original;
-
 	}
 }
