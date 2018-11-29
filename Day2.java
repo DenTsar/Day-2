69daf7935a2f636f933e35878157b1aa4d381239
DenTsar <45399002+DenTsar@users.noreply.github.com>
2018-11-29 12:09:29 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -6,7 +6,7 @@ public class Day2{
 	}
 	public static ArrayList<Integer> convertToArrayList(int a){
 		ArrayList<Integer> b = new ArrayList<>();
-		b.add(a%10);
+		b.add(0,a%10);
 		if(a/10!=0)
 			convertToArrayList(a/10);
 		return b;
