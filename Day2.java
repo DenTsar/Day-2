ed5565260240fd858ddc01bf00e0339e43752fc9
DenTsar <45399002+DenTsar@users.noreply.github.com>
2018-11-29 12:24:59 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -13,12 +13,8 @@ public class Day2{
 	}
 	public static int reconstruct(ArrayList<Integer> a){
 		int original = 0;
-		for (int i = 0; i<a.size(); i++){
-			if (i==0)
-				original+=a.get(i);
-			else
-				original+=(a.get(i)*10*i);
-		}
+		for (int i = 0; i<a.size(); i++)
+				original+=(a.get(i)*Math.pow(10,a.size()-i));
 		return original;
 	}
 }
