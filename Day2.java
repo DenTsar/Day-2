3f509fa96c81c3ac4b6847738ce70adfb23cbe97
akash10014285 <jainjakash@gmail.com>
2018-11-29 12:12:54 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
 public class Day2{
 	public static void main(String[]args){
-		System.out.println("Akash Jain");
-		System.out.println("GOAT");
-		System.out.println("Dennis Tsarkov");
+
 	}
 	public static ArrayList<Integer> convertToArrayList(int a){
 		ArrayList<Integer> b = new ArrayList<>();
@@ -11,5 +9,18 @@ public class Day2{
 		if(a/10!=0)
 			convertToArrayList(a/10);
 		return b;
+	}
+	public static int reconstruct(ArrayList<Integer> a){
+		int original = 0;
+
+		for (int i = 0; i<a.size(); i++){
+			if (i==0)
+				original+=a.get(i);
+			else
+				original+=(a.get(i)*10*i);
+		}
+
+		return original;
+
 	}
 }
