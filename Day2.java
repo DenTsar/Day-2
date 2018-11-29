95a2c668d6373e1971a15c5ec5cc026493845d3f
DenTsar <45399002+DenTsar@users.noreply.github.com>
2018-11-29 12:04:23 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
 public class Day2{
 	public static void main(String[]args){
 		System.out.println("Akash Jain");
 		System.out.println("GOAT");
 		System.out.println("Dennis Tsarkov");
-
+	}
+	public static ArrayList<Integer> convertToArrayList(int a){
+		ArrayList<Integer> b = new ArrayList<>();
+		b.add(a%10);
+		if(a/10!=0)
+			convertToArrayList(a/10);
+		return b;
 	}
 }
